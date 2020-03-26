b1c5a69481c72c08832c9af4fdc9b609bd0f3407
chic-caller-7520 <arbrown110@gmail.com>
2020-03-26 01:28:45 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,16 @@
-
-def speak_to_grandma("I LOVE YOU GRANDMA!")
+def speak_to_grandma
   puts "I LOVE YOU TOO PUMPKIN!"
-end
+if speak_to_grandma == "I LOVE YOU GRANDMA!"
+  puts "I LOVE YOU TOO PUMPKIN!" 
+elsif speak_to_grandma == "Hi Nana, how are you?"
+  puts "HUH?! SPEAK UP, SONNY!"
+elsif speak_to_grandma =="Hi!"
+  puts "HUH?! SPEAK UP, SONNY!"
+elsif speak_to_grandma =="WHAT DID YOU EAT TODAY?"
+  puts "NO, NOT SINCE 1938!"
+else speak_to_grandma == "WHAT?"
+  puts "NO, NOT SINCE 1938!"
+end  
+  
+
 
