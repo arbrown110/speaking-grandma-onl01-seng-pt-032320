2af2661955c828dce5dbbb71555676cde0e8805d
chic-caller-7520 <arbrown110@gmail.com>
2020-03-26 01:53:20 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,15 +2,15 @@ phrase = "I LOVE YOU GRANDMA!"
 
 def speak_to_grandma(phrase)
  if phrase =="I LOVE YOU GRANDMA!"
-   puts "I LOVE YOU TOO PUMPKIN!" 
+   print "LOVE YOU TOO PUMPKIN!" 
  elsif phrase =="Hi Nana, how are you?"
-   puts "HUH?! SPEAK UP, SONNY!"
+   print "HUH?! SPEAK UP, SONNY!"
  elsif phrase =="Hi!"
-   puts "HUH?! SPEAK UP, SONNY!"
+   print "HUH?! SPEAK UP, SONNY!"
  elsif phrase =="WHAT DID YOU EAT TODAY?"
-  puts "NO, NOT SINCE 1938!"
+  print "NO, NOT SINCE 1938!"
  else phrase =="WHAT?"
-   puts "NO, NOT SINCE 1938!"
+   print "NO, NOT SINCE 1938!"
  end  
 end  
 
