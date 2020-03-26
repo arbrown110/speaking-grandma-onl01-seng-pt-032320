d694e37df5b55240a634e577482a8dfafea945cd
chic-caller-7520 <arbrown110@gmail.com>
2020-03-26 02:27:36 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,9 +2,9 @@ def speak_to_grandma(phrase)
  if phrase =="I LOVE YOU GRANDMA!"
     "LOVE YOU TOO PUMPKIN!" 
 elsif phrase.upcase !=phrase
-  print"HUH?! SPEAK UP, SONNY!
-else
-  print "HUH?! SPEAK UP, SONNY!
+    "HUH?! SPEAK UP, SONNY!"
+elsif
+   "NO, NOT SINCE 1938!"
  end
 end  
 
