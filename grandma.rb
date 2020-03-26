c8d7f7004246efbd6c16ed2fca1aa9df2234b42a
chic-caller-7520 <arbrown110@gmail.com>
2020-03-26 02:25:53 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,8 @@
 def speak_to_grandma(phrase)
  if phrase =="I LOVE YOU GRANDMA!"
-   print "LOVE YOU TOO PUMPKIN!" 
+    "LOVE YOU TOO PUMPKIN!" 
 elsif phrase.upcase !=phrase
-  print "NO, NOT SINCE 1938!"
+  print"HUH?! SPEAK UP, SONNY!
 else
   print "HUH?! SPEAK UP, SONNY!
  end
